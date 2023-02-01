437f998b70b5fd488b536003de7669a7e8d514df
IvanUsatov <andrappdevelop@gmail.com>
2023-02-01 12:06:31 +0700

    Add uri

--- app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListProvider.kt
@@ -2,11 +2,17 @@ package com.example.shoppinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider : ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.example.shoppinglist", "shop_items", GET_SHOP_ITEMS_QUERY)
+    }
+
     override fun onCreate(): Boolean {
         return true
     }
@@ -18,7 +24,13 @@ class ShopListProvider : ContentProvider() {
         p3: Array<out String>?,
         p4: String?
     ): Cursor? {
-        Log.d("ShopListProvider", "query $p0")
+        val code = uriMatcher.match(p0)
+        when (code) {
+            GET_SHOP_ITEMS_QUERY -> {
+
+            }
+        }
+        Log.d("ShopListProvider", "query $p0 code $code")
         return null
     }
 
@@ -36,5 +48,9 @@ class ShopListProvider : ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val GET_SHOP_ITEMS_QUERY = 100
     }
 }
